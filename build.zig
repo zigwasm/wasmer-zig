467ed031993b5c5476037295d46388ba16d5058f
Jakub Konka <kubkon@jakubkonka.com>
2021-05-25 19:32:24 +0200

    Port wasmer C example

--- build.zig
+++ build.zig
@@ -1,16 +1,43 @@
 const std = @import("std");
+const mem = std.mem;
+
+fn detectWasmerLibDir(b: *std.build.Builder) ?[]const u8 {
+    const argv = &[_][]const u8{ "wasmer", "config", "--libdir" };
+    const result = std.ChildProcess.exec(.{
+        .allocator = b.allocator,
+        .argv = argv,
+    }) catch return null;
+
+    if (result.stderr.len != 0 or result.term.Exited != 0) return null;
+
+    const lib_dir = mem.trimRight(u8, result.stdout, "\r\n");
+    return lib_dir;
+}
 
 pub fn build(b: *std.build.Builder) !void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
+    // Try detecting Wasmer lib dir.
+    const wasmer_lib_dir = detectWasmerLibDir(b);
+
     const lib = b.addStaticLibrary("wasmer-zig", "src/main.zig");
     lib.setBuildMode(mode);
+    lib.linkSystemLibrary("wasmer");
+    if (wasmer_lib_dir) |lib_dir| {
+        lib.addLibPath(lib_dir);
+    }
+    lib.linkLibC();
     lib.install();
 
     var main_tests = b.addTest("src/main.zig");
     main_tests.setBuildMode(mode);
+    main_tests.linkSystemLibrary("wasmer");
+    if (wasmer_lib_dir) |lib_dir| {
+        main_tests.addLibPath(lib_dir);
+    }
+    main_tests.linkLibC();
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
@@ -26,6 +53,11 @@ pub fn build(b: *std.build.Builder) !void {
     const executable = b.addExecutable(example orelse "simple", example_path);
     executable.setBuildMode(mode);
     executable.addPackagePath("wasmer", "src/main.zig");
+    executable.linkSystemLibrary("wasmer");
+    if (wasmer_lib_dir) |lib_dir| {
+        executable.addLibPath(lib_dir);
+    }
+    executable.linkLibC();
     executable.step.dependOn(b.getInstallStep());
 
     const run_executable = executable.run();
--- examples/simple.zig
+++ examples/simple.zig
@@ -1,6 +1,27 @@
 const std = @import("std");
 const wasmer = @import("wasmer");
+const assert = std.debug.assert;
+const fs = std.fs;
 
-pub fn main() void {
-    std.log.info("{}", .{wasmer.add(1, 1)});
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const allocator = &gpa.allocator;
+
+pub fn main() !void {
+    const file = try fs.cwd().openFile("examples/sum.wasm", .{});
+    defer file.close();
+
+    const bytes = try file.readToEndAlloc(allocator, std.math.maxInt(u64));
+    defer allocator.free(bytes);
+
+    // Create new instance.
+    const instance = try wasmer.Instance.new(bytes, &.{});
+    defer instance.destroy();
+
+    // Prepare input args.
+    const args = &[_]wasmer.Value{ wasmer.Value.from_i32(7), wasmer.Value.from_i32(8) };
+    const result = (try instance.call("sum", args)).as_i32().?;
+
+    assert(result == 15);
+
+    std.log.info("sum.wasm: {} + {} == {}", .{ 7, 8, result });
 }
